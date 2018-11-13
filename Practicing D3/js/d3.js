d14ede03eadf8e802c19f6c19b98193485912559
Georgi <georgiddam@gmail.com>
2018-11-13 11:04:11 +0000

    Parsing passwords

--- Practicing D3/js/d3.js
+++ Practicing D3/js/d3.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
 var length = 0;
 var canvas = null;
 var created = false;
+var allPasswords = passwordData.allPasswords
     function svgExample() {
         canvas = d3.select(".container")
             .append("svg")
@@ -39,12 +40,12 @@ var created = false;
                 .append("circle")
                 .attr("fill", "blue")
                 .attr("class", "passCircle")
-                .attr("cx", function(d, i) {
-                    console.log("test", d, i);
+                .attr("cx", function(d, z) {
+                    // console.log("test", d, i);
                     return 50 + d*2;
                 })
                 .attr("cy", function (d, i) {
-                    console.log(d, i);
+                    // console.log(d, i);
                     return Math.random()*150;
                 })
                 .attr ("r", 5)
@@ -54,11 +55,31 @@ var created = false;
     }
 
     function getPassword() {
+
+
+
         $( ".password" ).keyup(function() {
           length = $(".password").val().length;
+          for (var i = 0; i<allPasswords.length; i++){
+              var lDistance = levenshteinDistance(passwordData.allPasswords[i].password, $(".password").val());
+          }
+          console.log(lDistance);
           update();
         });
     }
+
+    function levenshteinDistance (s, t) {
+        if (!s.length) return t.length;
+        if (!t.length) return s.length;
+
+        return Math.min(
+            levenshteinDistance(s.substr(1), t) + 1,
+            levenshteinDistance(t.substr(1), s) + 1,
+            levenshteinDistance(s.substr(1), t.substr(1)) + (s[0] !== t[0] ? 1 : 0)
+        ) + 1;
+    }
+
+    console.log(passwordData);
     getPassword();
     svgExample();
 
